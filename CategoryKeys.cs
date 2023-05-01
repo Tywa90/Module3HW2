fbfa9af0c162f16ba23e621cc07b4efff67a3cf2
Serhii Amelin <amserega90@gmail.com>
2023-05-01 11:15:17 +0300

    Add styleCop

--- CategoryKeys.cs
+++ CategoryKeys.cs
@@ -35,7 +35,6 @@ namespace Dictionary
             for (int i = 0; i < _alfabetUS.Length; i++)
             {
                 _tempList = new List<User>();
-
                 for (int j = 0; j < _userCollection.Count; j++)
                 {
                     _key = _userCollection[j].Name[0].ToString();
@@ -63,8 +62,10 @@ namespace Dictionary
                 {
                     _tempList.Add(_userCollection[i]);
                 }
+
                 _key = _keyDigit;
             }
+
             if (_tempList.Count > 0)
             {
                 _dictionary.Add(_key, _tempList);
@@ -75,40 +76,14 @@ namespace Dictionary
         {
             _tempList = new List<User>();
 
-            //var keyList = new List<string>();
-            //for (int i = 0; i < _userCollection.Count; i++)
-            //{
-            //    _key = _userCollection[i].Name[0].ToString();
-            //    keyList.Add(_key);
-            //}
-            //keyList.Sort();
-            //_alfabetUS = _alfabetUS + "1234567890";
-
-            //foreach (var item in keyList)
-            //{
-            //    for (int i = 0; i < _alfabetUS[i]; i++)
-            //    {
-            //        if (item.StartsWith(_alfabetUS[i]))
-            //        {
-            //            Console.WriteLine("Enter");
-            //        }
-            //    }
-
-            //}
-
-            //foreach (string key in keyList)
-            //{
-            //    Console.WriteLine(key);
-            //}
-            //Console.WriteLine("-------------");
-
+            // something wrong
             var alfabetString = _alfabetUS + "1234567890";
             for (int i = 0; i < _userCollection.Count; i++)
             {
                 for (int j = 0; j < alfabetString.Length; j++)
                 {
                     _key = _userCollection[i].Name[0].ToString();
-                    if (!_key.StartsWith(alfabetString[j].ToString()))
+                    if (!_key.StartsWith(alfabetString[j].ToString()) && !int.TryParse(_key, out int number))
                     {
                         _tempList.Add(_userCollection[i]);
                     }
--- Program.cs
+++ Program.cs
@@ -8,29 +8,11 @@ namespace Dictionary
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //var phoneBook = new PhoneBook<Contact>();
-            //phoneBook.Add(new Contact() { FirstName = "Alena", LastName = "Belkova"});
-            //phoneBook.Add(new Contact() { FirstName = "Bob", LastName = "Muray" });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-           Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.OutputEncoding = UTF8Encoding.UTF8;
             var cultureUS = new CultureInfo("en-US");
             var cultureUA = new CultureInfo("uk-UA");
-
 
             var userCollection = new List<User>();
             userCollection.Add(new User() { Name = "*Андрій" });
@@ -45,106 +27,12 @@ namespace Dictionary
             userCollection.Add(new User() { Name = "8Miles" });
             userCollection.Add(new User() { Name = "Alex" });
 
-
-
             userCollection.Sort();
 
             var dictionary = new Dictionary<string, List<User>>();
-           
-            //string alfabetUS = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
-            //string keyDigit = "0-9";
-            //string keyOthers = "#";
 
             CategoryKeys keys = new CategoryKeys(userCollection, dictionary);
             keys.KeysCreat();
-
-
-
-            //for (int i = 0; i < alfabetUS.Length; i++)
-            //{
-            //    var tempList = new List<User>();
-
-            //    for (int j = 0; j < userCollection.Count; j++)
-            //    {
-            //        key = userCollection[j].Name[0].ToString();
-            //        if (key.Contains(alfabetUS[i]))
-            //        {
-            //            tempList.Add(userCollection[j]);
-            //        }
-            //    }
-
-            //    key = alfabetUS[i].ToString();
-            //    if (tempList.Count > 0)
-            //    {
-            //        dictionary.Add(key, tempList);
-            //    }
-            //}
-
-            //var tempDigitList = new List<User>();
-            //for (int i = 0; i < userCollection.Count; i++)
-            //{
-            //    key = userCollection[i].Name[0].ToString();
-            //    if (int.TryParse(key, out int res))
-            //    {
-            //        tempDigitList.Add(userCollection[i]);
-            //    }
-            //    key = keyDigit;
-            //}
-            //if (tempDigitList.Count > 0)
-            //{
-            //    dictionary.Add(key, tempDigitList);
-            //}
-
-
-
-            //var tempOthersList = new List<User>();
-            //for (int i = 0; i < userCollection.Count; i++)
-            //{
-            //    key = userCollection[i].Name[0].ToString();
-
-            //        if (!key.Equals(alfabetUS[i]) && !int.TryParse(key, out int res1))
-            //        {
-            //            tempOthersList.Add(userCollection[i]);
-            //        }
-
-            //    key = keyOthers;
-            //}
-            //if (tempOthersList.Count > 0)
-            //{
-            //    dictionary.Add(key, tempOthersList);
-            //}
-
-            //for (int i = 0; i < userCollection.Count; i++)
-            //{
-            //    key = userCollection[i].Name[0].ToString();
-            //    var tempList = new List<User>();
-
-            //    if (key.Contains(alfabetUS[i]))
-            //    {
-
-            //        tempList.Add(userCollection[i]);
-            //        key = alfabetUS[i].ToString();
-
-            //if (!dictionary.ContainsKey(key))
-            //{
-
-            //}
-            //dictionary.Add(key, userCollection[i]);
-            //    }
-
-
-
-
-            //}
-
-            //dictionary.Add("I", userCollection);
-            //dictionary.Add("E", userCollection);
-
-            //foreach (var key in dictionary)
-            //{
-            //    key.Key.ToList().Sort();
-            //}
-
 
             foreach (var item in dictionary)
             {
@@ -159,8 +47,6 @@ namespace Dictionary
                     Console.WriteLine(item.Value[i]);
                 }
             }
-
-
 
             Console.WriteLine();
         }
