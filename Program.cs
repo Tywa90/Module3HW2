ad6a68534db463049c58f184a707eefb0840e3be
Serhii Amelin <amserega90@gmail.com>
2023-05-02 16:48:02 +0300

    del comments

--- Program.cs
+++ Program.cs
@@ -10,9 +10,6 @@ namespace Dictionary
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine($"Current culture: {CultureInfo.CurrentCulture}");
-            Console.WriteLine($"Current UIculture: {CultureInfo.CurrentUICulture}");
-
             Console.OutputEncoding = UTF8Encoding.UTF8;
             var cultureUS = new CultureInfo("en-US");
             var cultureUA = new CultureInfo("uk-UA");
@@ -51,19 +48,6 @@ namespace Dictionary
                     Console.WriteLine(item.Value[i]);
                 }
             }
-            //// Display the name of the current culture.
-            //Console.WriteLine("CurrentCulture is {0}.", CultureInfo.CurrentCulture.Name);
-
-            //// Change the current culture to th-TH.
-            //CultureInfo.CurrentCulture = new CultureInfo("th-TH", false);
-            //Console.WriteLine("CurrentCulture is now {0}.", CultureInfo.CurrentCulture.Name);
-
-            //// Display the name of the current UI culture.
-            //Console.WriteLine("CurrentUICulture is {0}.", CultureInfo.CurrentUICulture.Name);
-
-            //// Change the current UI culture to ja-JP.
-            //CultureInfo.CurrentUICulture = new CultureInfo("ja-JP", false);
-            //Console.WriteLine("CurrentUICulture is now {0}.", CultureInfo.CurrentUICulture.Name);
         }
     }
 }
